03aa9aabc9170268f408f726ef468e50404568a3
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2012-07-26 18:15:46 +0200

    CLEANUP: Properly merge options in gzopen/4.  Clarify encoding issues in documentation.

--- zlib.pl
+++ zlib.pl
@@ -1,11 +1,9 @@
-/*  $Id$
-
-    Part of SWI-Prolog
+/*  Part of SWI-Prolog
 
     Author:        Jan Wielemaker
     E-mail:        J.Wielemaker@cs.vu.nl
     WWW:           http://www.swi-prolog.org
-    Copyright (C): 1985-2011, University of Amsterdam
+    Copyright (C): 1985-2012, University of Amsterdam
 			      VU University Amsterdam
 
     This program is free software; you can redistribute it and/or
@@ -67,23 +65,26 @@ Read/write compressed data based on the zlib library.
 %
 %	Open a file compatible with the  gzip   program.  Note that if a
 %	file is opened in =append= mode,  a   second  gzip image will be
-%	added to the end of the file.
+%	added to the end of the file.   The gzip standard defines that a
+%	file can hold multiple  gzip  images   and  inflating  the  file
+%	results in a concatenated stream of all inflated images.
 %
-%	@tbd	Later versions may actually append to an existing
-%		compressed file.
+%	Options are passed to open/4  and   zopen/3.  Default  format is
+%	=gzip=.
 
 gzopen(File, Mode, Stream) :-
 	gzopen(File, Mode, Stream, []).
 
 gzopen(File, Mode, Stream, Options) :-
 	must_be(oneof([read,write,append]), Mode),
-	partition(zoption, Options, ZOptions, OpenOptions),
+	partition(zoption, Options, ZOptions0, OpenOptions),
+	merge_options(ZOptions0,
+		      [ format(gzip),
+			close_parent(true)
+		      ], ZOptions),
 	open(File, Mode, Stream0, OpenOptions),
-	zopen(Stream0, Stream,
-	      [ format(gzip),
-		close_parent(true)
-	      | ZOptions
-	      ]).
+	zopen(Stream0, Stream, ZOptions).
+
 
 zoption(format(_)).
 zoption(multi_part(_)).
